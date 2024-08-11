de48dddeed21e47e5a3dd796d462b8620017c2af
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-08-12 00:02:56 +0530

    js promise

--- web/js/setTimeOut/index.js
+++ web/js/setTimeOut/index.js
@@ -3,20 +3,21 @@
 //     console.log("another hello");
 // }
 
-function greet(func,greetTime){
-    console.log("hii")
-    setTimeout(func,greetTime);
-}
+// function greet(func,greetTime){
+//     console.log("hii")
+//     setTimeout(func,greetTime);
+// }
 
 
 function setTimeoutPromisified(ms){
     return new Promise(function(resolve,reject){
-        greet(resolve,ms);
+        console.log("wait")
+        setTimeout(resolve,ms);
     });
 }
 
 function callback(){
-    console.log("3 seconds have passed");
+    console.log("3 seconds have passed,go");
 }
 
 setTimeoutPromisified(3000).then(callback);
