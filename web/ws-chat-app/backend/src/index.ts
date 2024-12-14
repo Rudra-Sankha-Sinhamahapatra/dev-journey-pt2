5b6271a666481029baf32b16203e605ca0e618a2
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-12-14 17:36:03 +0530

    rooms using ws logic added

--- web/ws-chat-app/backend/src/index.ts
+++ web/ws-chat-app/backend/src/index.ts
@@ -1,33 +1,45 @@
-import { WebSocketServer } from "ws";
-import WebSocket from "ws";
+import { WebSocketServer,WebSocket } from "ws";
 
 const wss = new WebSocketServer({port:8080});
 
+let users = 0;
+interface User {
+  socket:WebSocket;
+  room:string;
+}
+let allSockets:User[] = [];
 wss.on("connection",function (socket){
-  console.log("user connected");
-
-  
-
   socket.on('message',(e)=>{
     try {
       const messageData = JSON.parse(e.toString());
-
-    if (messageData.message==='exit'){
-        socket.close();
-    }
-    else {
-        wss.clients.forEach((client)=>{
-          if(client.readyState===WebSocket.OPEN){
-            client.send(JSON.stringify(messageData))
+      if(messageData.type === 'join'){
+        console.log('user joined room',messageData.payload.roomId)
+        allSockets.push({
+          socket:socket,
+          room:messageData.payload.roomId
+        })
+      }
+  
+      if(messageData.type === 'chat'){
+        console.log("User wants to chat")
+        const currentUserRoom = allSockets.find((x)=> x.socket === socket)?.room;
+        allSockets.forEach((x)=>{
+          if(x.room === currentUserRoom){
+            x.socket.send(messageData.payload.message);
           }
         })
-    }
+      }
+       
   } catch(error) {
     console.log("Invalid message format",error);
   }
   })
   socket.on('close',() => {
     console.log("User disconnected");
+  })
+  socket.on('disconnect',()=>{
+    users = users - 1;
+    allSockets = allSockets.filter(x=> x.socket!=socket)
   })
 }
 )
